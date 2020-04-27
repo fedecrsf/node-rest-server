e468b9dae99ae3145b85ee1066fb7410ff506b9d
Federico Garcia <fedecrsf@gmail.com>
2020-04-27 11:26:21 -0300

    ENV

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if (process.env.NODE_ENV = 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cursonode:JMWeFsfFQ6zbXEq5@cluster0-z0fxa.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URL_DB = urlDB;
